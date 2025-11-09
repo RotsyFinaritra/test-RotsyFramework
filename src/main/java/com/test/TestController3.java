530d7ba45a81f071b59e1fbc0f29efab49ce918e
Randriamifidisoa Rotsy Finaritra <rotsyfinaritra0@gmail.com>
2025-11-09 13:45:57 +0300

    test sprint 2 sy 3

--- src/main/java/com/test/TestController3.java
+++ src/main/java/com/test/TestController3.java
@@ -1,8 +1,12 @@
 package com.test;
 
 import com.etu003184.annotation.Controller;
+import com.etu003184.annotation.UrlMapping;
 
 @Controller
 public class TestController3 {
-    
+    @UrlMapping("/mirana")
+    public String mirana() {
+        return "Hello from TestController3";
+    }
 }
