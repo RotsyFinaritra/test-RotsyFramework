ea8abb9ce3fe3dfe2178174b39545f3a67600ed5
Randriamifidisoa Rotsy Finaritra <rotsyfinaritra0@gmail.com>
2025-11-09 15:35:40 +0300

    test sprint4
    manampy url sy methode mamerina string sy ModelView de nanampy page koa.

--- src/main/java/com/test/TestController1.java
+++ src/main/java/com/test/TestController1.java
@@ -2,6 +2,7 @@ package com.test;
 
 import com.etu003184.annotation.Controller;
 import com.etu003184.annotation.UrlMapping;
+import com.etu003184.model.ModelView;
 
 @Controller
 public class TestController1 {
@@ -9,6 +10,12 @@ public class TestController1 {
     @UrlMapping("/hello")
     public String hello() {
         return "Hello from TestController1";
+    }
+
+    @UrlMapping("/aaa")
+    public ModelView testModelView() {
+        ModelView modelView = new ModelView("views/aaa.html");
+        return modelView;
     }
     
 }
